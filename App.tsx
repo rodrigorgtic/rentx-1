ad9277a8046844c405e6d8096b7725fcce9f3f7a
Júnior Nunes <denisonprobable13@gmail.com>
2021-06-29 12:37:52 -0300

    google fonts installed

--- App.tsx
+++ App.tsx
@@ -1,5 +1,18 @@
 import React from 'react';
-import { Home } from './scr/screens';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium
+} from '@expo-google-fonts/inter'
+
+import {
+  Archivo_400Regular,
+  Archivo_500Medium,
+  Archivo_600SemiBold
+} from '@expo-google-fonts/archivo';
+
+
+import { Home } from './src/screens';
 
 export default function App() {
   return (
