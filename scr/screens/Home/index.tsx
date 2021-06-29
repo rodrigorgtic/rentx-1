40598684ec00cea0b6d032f0f297ff83acb1aa48
Júnior Nunes <denisonprobable13@gmail.com>
2021-06-29 12:20:23 -0300

    Styled-Components installed

---
+++ scr/screens/Home/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import {
+    Container,
+    Title
+} from './styles'
+
+interface Props{
+    title: string;
+}
+
+export function Home({ title } : Props){
+
+    return (
+        <Container>
+            <Title> {title} </Title>
+        </Container>
+    );
+}
