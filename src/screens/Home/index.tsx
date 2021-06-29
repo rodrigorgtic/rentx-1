cb816fcfa0b3fe7c45584879bd7ab64f3fb36b7a
Júnior Nunes <denisonprobable13@gmail.com>
2021-06-29 15:36:57 -0300

    react-native-svg and react-native-svg-transformer libs installed

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
+
+import Logo from '../../assets/logo.svg';
 
 import {
     Container,
-    Title
+    Header
 } from './styles'
 
-interface Props{
-    title: string;
-}
 
 export function Home(){
 
     return (
         <Container>
-            <Title> Home </Title>
+            <StatusBar 
+                barStyle="light-content"
+                backgroundColor="transparent"
+                translucent
+            />
+            <Header>
+                <Logo />
+            </Header>
         </Container>
     );
 }
