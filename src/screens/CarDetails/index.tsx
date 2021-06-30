c905afc9819dacddae8e9769b0788ac6d02cfb31
Júnior Nunes <denisonprobable13@gmail.com>
2021-06-30 15:01:16 -0300

    continuing CarDetails Screen

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -8,7 +8,16 @@ import {
 import {
     Container,
     Header,
-    CardImage
+    CardImage,
+    Content,
+    Details,
+    Description,
+    Brand,
+    Name,
+    Rent,
+    Period,
+    Price,
+    About
 } from './styles'
 
 
@@ -27,6 +36,29 @@ export function CarDetails(){
                     imagesUrl={['https://pictures.dealer.com/a/audibrooklynnyaoa/1927/20995e45b1f28b176b0fccb0309e9e7ax.jpg?impolicy=downsize&w=568']}
                 />
             </CardImage>
+
+            <Content>
+                <Details>
+                    <Description>
+                        <Brand>Lamborghini</Brand>
+                        <Name>Huracan</Name>
+                    </Description>
+
+                    <Rent>
+                        <Period>Ao dia</Period>
+                        <Price>R$ 580</Price>
+                    </Rent>
+
+                </Details>
+
+                <About>
+                    Este é automóvel desportivo. Surgiu do
+                    lendário touro de lide indultado na praça Real
+                    Maestranza de Sevilla. É um belíssimo carro 
+                    para quem gosta de acelerar.
+                </About>
+
+            </Content>
 
         </Container>
     );
