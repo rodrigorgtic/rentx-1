13a20a2104b3662508da0635af42aadc16354f02
Júnior Nunes <denisonprobable13@gmail.com>
2021-07-05 17:08:29 -0300

    Showing api content

---
+++ src/dtos/CarDTO.ts
@@ -0,0 +1,21 @@
+interface IRent{
+    period: string;
+    price: string;
+}
+
+interface IAccessories{
+    type: string;
+    name: string;
+}
+
+export interface CarDTO {
+    id: string;
+    brand: string;
+    name: string;
+    about: string;
+    rent: IRent;
+    fuel_type: string;
+    thumbnail: string;
+    accessories: IAccessories[];
+    photos: string[];
+  }
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { StatusBar } from 'react-native';
 
 import { RFValue } from 'react-native-responsive-fontsize';
 
 import Logo from '../../assets/logo.svg';
+import api from '../../services/api';
+import { CarDTO } from '../../dtos/CarDTO';
 import { Car, CarProps } from '../../components';
 
 import {
@@ -17,9 +19,11 @@ import {
 
 
 export function Home(){
-    const navigation =useNavigation();
+    const [ cars, setCars ] = useState<CarDTO[]>([] as CarDTO[]);
+    const [ loading, setLoading ] = useState(true);
+    const navigation = useNavigation();
 
-    const cars: CarProps = {
+    const carData: CarProps = {
             brand: 'Audi',
             name: 'RS 5 Coupé',
             rent: {
@@ -31,7 +35,21 @@ export function Home(){
 
     function handleCarDetails(){
         navigation.navigate('CarDetails');
-    }
+    };
+
+    useEffect(() => {
+        async function fetchCars() {
+            try {
+                const response = await api.get('/cars');
+                setCars(response.data);
+            } catch (error) {
+                console.log(error);
+            } finally {
+                setLoading(false);
+            }
+        }
+        fetchCars();
+    }, []);
 
     return (
         <Container>
@@ -54,11 +72,11 @@ export function Home(){
             </Header>
 
             <CarList 
-                data={[1, 2, 3, 4, 5, 6, 7]}
-                keyExtractor={item => String(item)}
+                data={cars}
+                keyExtractor={item => item.id}
                 renderItem={({ item }) => (
                     <Car 
-                        data={cars}
+                        data={item}
                         onPress={handleCarDetails}
                     />
                 )}
