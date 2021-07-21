3d6be5f525e1bb27ddaa162c0beb4f789d7d2c02
Júnior Nunes <denisonprobable13@gmail.com>
2021-07-21 02:56:43 -0300

    FirstStep Screen created

--- src/screens/SingUp/SingUpFirstStep/index.tsx
+++ src/screens/SingUp/SingUpFirstStep/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import {
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
-import { BackButton, Bullet } from '../../../components';
+import { BackButton, Bullet, Input, Button } from '../../../components';
 
 import {
   Container,
@@ -21,25 +26,33 @@ export function SingUṕFirstStep() {
   }
 
   return (
-    <Container>
-      <Header>
-        <BackButton onPress={handleBack} />
-        <Steps>
-          <Bullet active />
-          <Bullet />
-        </Steps>
-      </Header>
-      <Title>
-        Crie sua {'\n'}
-        conta
-      </Title>
-      <Subtitle>
-        Faça seu cadastro de {'\n'}
-        forma rápida e fácil
-      </Subtitle>
-      <Form>
-        <FormTitle>1. Dados</FormTitle>
-      </Form>
-    </Container>
+    <KeyboardAvoidingView behavior="position" enabled>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <Container>
+          <Header>
+            <BackButton onPress={handleBack} />
+            <Steps>
+              <Bullet active />
+              <Bullet />
+            </Steps>
+          </Header>
+          <Title>
+            Crie sua {'\n'}
+            conta
+          </Title>
+          <Subtitle>
+            Faça seu cadastro de {'\n'}
+            forma rápida e fácil
+          </Subtitle>
+          <Form>
+            <FormTitle>1. Dados</FormTitle>
+            <Input iconName="user" placeholder="Nome" />
+            <Input iconName="mail" placeholder="E-mail" />
+            <Input iconName="credit-card" placeholder="CNH" />
+          </Form>
+          <Button title="Próximo" />
+        </Container>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 }
