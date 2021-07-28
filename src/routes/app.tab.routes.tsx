070ab8965c7d7e666fe21525246ad3fbe834fc08
Júnior Nunes <denisonprobable13@gmail.com>
2021-07-28 19:02:06 -0300

    Using icons with BottomTabNavigator

--- src/routes/app.tab.routes.tsx
+++ src/routes/app.tab.routes.tsx
@@ -2,17 +2,65 @@ import React from 'react';
 
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
+import { useTheme } from 'styled-components';
+import { Platform } from 'react-native';
+import HomeSvg from '../assets/home.svg';
+import CarSvg from '../assets/car.svg';
+import PeopleSvg from '../assets/people.svg';
+
 import { Home, MyCars } from '../screens';
 import { AppStackRoutes } from './app.stack.routes';
+
+interface TabBarIconProps {
+  color: string;
+}
 
 const { Navigator, Screen } = createBottomTabNavigator();
 
 export function AppTabRoutes() {
+  const theme = useTheme();
+  const { main, text_detail, background_primary } = theme.colors;
+
   return (
-    <Navigator>
-      <Screen name="Home" component={AppStackRoutes} />
-      <Screen name="Profile" component={Home} />
-      <Screen name="MyCars" component={MyCars} />
+    <Navigator
+      tabBarOptions={{
+        activeTintColor: main,
+        inactiveTintColor: text_detail,
+        showLabel: false,
+        style: {
+          paddingVertical: Platform.OS === 'ios' ? 20 : 0,
+          height: 75,
+          backgroundColor: background_primary,
+        },
+      }}
+    >
+      <Screen
+        name="Home"
+        component={AppStackRoutes}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <HomeSvg width={24} height={24} fill={color} />
+          ),
+        }}
+      />
+      <Screen
+        name="Profile"
+        component={Home}
+        options={{
+          tabBarIcon: ({ color }: TabBarIconProps) => (
+            <CarSvg width={24} height={24} fill={color} />
+          ),
+        }}
+      />
+      <Screen
+        name="MyCars"
+        component={MyCars}
+        options={{
+          tabBarIcon: ({ color }: TabBarIconProps) => (
+            <PeopleSvg width={24} height={24} fill={color} />
+          ),
+        }}
+      />
     </Navigator>
   );
 }
