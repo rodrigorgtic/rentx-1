c0ff5223643e022e5e1b72c54021c0f74d054d8b
Júnior Nunes <denisonprobable13@gmail.com>
2021-07-31 22:00:25 -0300

    Header of Profile screen created

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -1,15 +1,51 @@
 import React from 'react';
+import { useTheme } from 'styled-components';
+import { useNavigation } from '@react-navigation/native';
+import { Feather } from '@expo/vector-icons';
 
-import { Container, Title } from './styles';
+import { BackButton } from '../../components';
 
-interface Props {
-  title: string;
-}
+import {
+  Container,
+  Header,
+  HeaderTop,
+  HeaderTitle,
+  LogoutButton,
+  PhotoContainer,
+  Photo,
+  PhotoButton,
+} from './styles';
 
-export function Profile({ title }: Props) {
+export function Profile() {
+  const theme = useTheme();
+  const { shape } = theme.colors;
+  const navigaton = useNavigation();
+
+  function handleBack() {
+    navigaton.goBack();
+  }
+
+  function handleSingOut(): void {}
+
   return (
     <Container>
-      <Title> {title} </Title>
+      <Header>
+        <HeaderTop>
+          <BackButton color={shape} onPress={handleBack} />
+          <HeaderTitle>Editar Perfil</HeaderTitle>
+          <LogoutButton onPress={handleSingOut}>
+            <Feather name="power" size={24} color={shape} />
+          </LogoutButton>
+        </HeaderTop>
+
+        <PhotoContainer>
+          <Photo source={{ uri: 'https://github.com/jnunes-ds.png' }} />
+
+          <PhotoButton onPress={() => {}}>
+            <Feather name="camera" size={24} color={shape} />
+          </PhotoButton>
+        </PhotoContainer>
+      </Header>
     </Container>
   );
 }
